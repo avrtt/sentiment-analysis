7803f88b69dc078a2fdcc63cbb0ae001d10ff96f
Vladislav <lenferdetroud@gmail.com>
2021-11-09 16:02:11 +0700

    Update sentiment_classifier.py

--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -10,71 +10,18 @@ class SentimentClassifier(object):
     def get_probability_words(probability):
         if round(probability, 2) < 0.70:
             return 'confidence: < 70% (probably neutral)'
-
-        ''' it works, don't touch '''
         
+        '''
         if round(probability, 2) == 0.71:
             return 'confidence: 71%'
-        if round(probability, 2) == 0.72:
-            return 'confidence: 72%'
-        if round(probability, 2) == 0.73:
-            return 'confidence: 73%'
-        if round(probability, 2) == 0.74:
-            return 'confidence: 74%'
-        if round(probability, 2) == 0.75:
-            return 'confidence: 75%'
-        if round(probability, 2) == 0.76:
-            return 'confidence: 76%'
-        if round(probability, 2) == 0.77:
-            return 'confidence: 77%'
-        if round(probability, 2) == 0.78:
-            return 'confidence: 78%'
-        if round(probability, 2) == 0.79:
-            return 'confidence: 79%'
-        if round(probability, 2) == 0.80:
-            return 'confidence: 80%'
-        if round(probability, 2) == 0.81:
-            return 'confidence: 81%'
-        if round(probability, 2) == 0.82:
-            return 'confidence: 82%'
-        if round(probability, 2) == 0.83:
-            return 'confidence: 83%'
-        if round(probability, 2) == 0.84:
-            return 'confidence: 84%'
-        if round(probability, 2) == 0.85:
-            return 'confidence: 85%'
-        if round(probability, 2) == 0.86:
-            return 'confidence: 86%'
-        if round(probability, 2) == 0.87:
-            return 'confidence: 87%'
-        if round(probability, 2) == 0.88:
-            return 'confidence: 88%'
-        if round(probability, 2) == 0.89:
-            return 'confidence: 89%'
-        if round(probability, 2) == 0.90:
-            return 'confidence: 90%'
-        if round(probability, 2) == 0.91:
-            return 'confidence: 91%'
-        if round(probability, 2) == 0.92:
-            return 'confidence: 92%'
-        if round(probability, 2) == 0.93:
-            return 'confidence: 93%'
-        if round(probability, 2) == 0.94:
-            return 'confidence: 94%'
-        if round(probability, 2) == 0.95:
-            return 'confidence: 95%'
-        if round(probability, 2) == 0.96:
-            return 'confidence: 96%'
-        if round(probability, 2) == 0.97:
-            return 'confidence: 97%'
-        if round(probability, 2) == 0.98:
-            return 'confidence: 98%'
-        if round(probability, 2) == 0.99:
-            return 'confidence: 99%'
+            
+            ... // that was the shittiest code i've ever written, do for instead 
+        
         if round(probability, 2) == 1.00:
             return 'confidence: 100%'
         else:
             return 'undefined confidence'
+            '''
 
     def predict_text(self, text):
         try:
